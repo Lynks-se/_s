1419bca18b77efa3f36a374618a2e7dc0feb3996
Niklas Brunberg <n.brunberg@gmail.com>
2014-07-01 20:37:58 +0200

    Prepare header.php
    Add custom code to header.php, including IE Edge, meta viewport,
    respond stylesheets, and Bootstrap-compatible menu walker

--- header.php
+++ header.php
@@ -10,12 +10,18 @@
 <html <?php language_attributes(); ?>>
 <head>
 <meta charset="<?php bloginfo( 'charset' ); ?>">
-<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="viewport" content="width=device-width, initial-scale=1, minimal-ui">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
 <title><?php wp_title( '|', true, 'right' ); ?></title>
 <link rel="profile" href="http://gmpg.org/xfn/11">
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 
 <?php wp_head(); ?>
+<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+<!--[if lt IE 9]>
+	<script src="<?php echo get_stylesheet_directory_uri(); ?>/js/html5shiv.min.js"></script>
+	<script src="<?php echo get_stylesheet_directory_uri(); ?>/js/respond.min.js"></script>
+<![endif]-->
 </head>
 
 <body <?php body_class(); ?>>
@@ -28,9 +34,35 @@
 			<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
 		</div>
 
-		<nav id="site-navigation" class="main-navigation" role="navigation">
-			<button class="menu-toggle"><?php _e( 'Primary Menu', '_s' ); ?></button>
-			<?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
+		<nav id="site-navigation" class="navbar navbar-default main-navigation" role="navigation">
+			<div class="container-fluid">
+				<!-- Brand and toggle get grouped for better mobile display -->
+				<div class="navbar-header">
+					<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+						<span class="sr-only">Toggle navigation</span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					</button>
+					<a class="navbar-brand" href="<?php echo home_url(); ?>">
+						<?php bloginfo('name'); ?>
+					</a>
+				</div>
+
+				<?php
+					wp_nav_menu( array(
+						'menu'				=> 'primary',
+						'theme_location'	=> 'primary',
+						'depth'				=> 2,
+						'container'			=> 'div',
+						'container_class'	=> 'collapse navbar-collapse',
+						'container_id'		=> 'bs-example-navbar-collapse-1',
+						'menu_class'		=> 'nav navbar-nav',
+						'fallback_cb'		=> 'wp_bootstrap_navwalker::fallback',
+						'walker'			=> new wp_bootstrap_navwalker())
+					);
+				?>
+			</div>
 		</nav><!-- #site-navigation -->
 	</header><!-- #masthead -->
 
